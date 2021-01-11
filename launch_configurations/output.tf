e86f8c7637f8c9ea64bf0ac87599701b55688971
Antoine Qu'hen <antoinequhen@gmail.com>
2021-01-11 20:20:57 +0100

    Formatting

---
+++ launch_configurations/output.tf
@@ -0,0 +1,7 @@
+output "webapp_lc_id" {
+  value = aws_launch_configuration.webapp_lc.id
+}
+
+output "webapp_lc_name" {
+  value = aws_launch_configuration.webapp_lc.name
+}
--- site/variables.tf
+++ site/variables.tf
@@ -9,22 +9,21 @@
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under the License.
 variable "ip_range" {
-  default = "0.0.0.0/0" # Change to your IP Range!
 }
+
 variable "availability_zones" {
   # No spaces allowed between az names!
   default = []
 }
+
 variable "vpc_cidr" {
   description = "CIDR for the whole VPC"
-  default     = "10.0.0.0/16"
 }
+
 variable "public_subnet_cidr" {
   description = "CIDR for the Public Subnet"
-  default     = "10.0.0.0/24"
 }
+
 variable "private_subnet_cidr" {
   description = "CIDR for the Private Subnet"
-  default     = "10.0.1.0/24"
 }
-variable "key_name" {}
