c81616533c16994b397841288f5d5091992c4a6a
Antoine Qu'hen <antoinequhen@gmail.com>
2021-01-11 20:50:32 +0100

    Formatting

--- autoscaling_groups/variables.tf
+++ autoscaling_groups/variables.tf
@@ -8,16 +8,12 @@
 # or in the "license" file accompanying this file. This file is distributed on an "AS IS"
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under the License.
-variable "availability_zones" {
-  # No spaces allowed between az names!
-  default = ["us-west-2a","us-west-2b","us-west-2c"]
-}
-variable "asg_min" {
-  default = "2"
-}
-variable "asg_max" {
-  default = "10"
-}
+variable "availability_zones" {}
+
+variable "asg_min" {}
+
+variable "asg_max" {}
+
 #
 # From other modules
 #
